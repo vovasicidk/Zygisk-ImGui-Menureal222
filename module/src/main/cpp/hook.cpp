a42e0d0efa31073b3726be2d8c4fa77378a0bf28
vovasicidk <165401281+vovasicidk@users.noreply.github.com>
2024-04-14 21:52:52 +0300

    Update hook.cpp

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -31,7 +31,7 @@
 #include "Rect.h"
 #include <fstream>
 #include <limits>
-#define GamePackageName "com.kakaogames.gdts" // define the game package name here please
+#define GamePackageName "com.axlebolt.standoff2" // define the game package name here please
 
 int glHeight, glWidth;
 
